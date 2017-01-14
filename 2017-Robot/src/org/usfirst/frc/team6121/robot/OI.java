33abf6afdd973ad9af850f0213b429a8c2be07c7
beardo17 <07beardo@gmail.com>
2017-01-14 11:38:00 -0500

    Driving thing

--- 2017-Robot/src/org/usfirst/frc/team6121/robot/OI.java
+++ 2017-Robot/src/org/usfirst/frc/team6121/robot/OI.java
@@ -1,38 +1,35 @@
 package org.usfirst.frc.team6121.robot;
 
+import org.usfirst.frc.team6121.robot.commands.DriveForward;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
-import org.usfirst.frc.team6121.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	
+	public static Joystick xboxController;
+	
+	OI() {
+		
+		xboxController = new Joystick(RobotMap.XBOX_CONTROLLER);
+		
+		Button driveButton = new JoystickButton(xboxController, RobotMap.A_BUTTON);
+		driveButton.whileHeld(new DriveForward());
+		
+	}
+	
+	public double getY() {
+		return xboxController.getRawAxis(RobotMap.Y_AXIS);
+	}
+	
+	public double getX() {
+		return xboxController.getRawAxis(RobotMap.X_AXIS);
+	}
+	
 }
 
---
+++ 2017-Robot/src/org/usfirst/frc/team6121/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,37 @@
+
+package org.usfirst.frc.team6121.robot.subsystems;
+
+import org.usfirst.frc.team6121.robot.Robot;
+import org.usfirst.frc.team6121.robot.RobotMap;
+import org.usfirst.frc.team6121.robot.commands.ArcadeDrive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public double x = Robot.oi.getX();
+	public double y = Robot.oi.getY();
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new ArcadeDrive());
+    }
+    
+    public void arcadeDrive(Joystick stick) {
+    	RobotMap.driveTrain.arcadeDrive(y, -x, false);
+    }
+    
+    public void driveForward(double a) {
+    	RobotMap.driveTrain.setLeftRightMotorOutputs(a, a);
+    }
+    
+}
+
