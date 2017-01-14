dc34ec16bec51abfa0dd1f18839bcd3ace99b93c
beardo17 <07beardo@gmail.com>
2017-01-14 11:38:00 -0500

    Driving thing

--- 2017-Robot/src/org/usfirst/frc/team6121/robot/Robot.java
+++ 2017-Robot/src/org/usfirst/frc/team6121/robot/Robot.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team6121.robot.commands.ExampleCommand;
-import org.usfirst.frc.team6121.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team6121.robot.commands.ArcadeDrive;
+import org.usfirst.frc.team6121.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final DriveSubsystem driveSubsystem = new DriveSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
@@ -32,7 +32,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
 		oi = new OI();
         chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
+        chooser.addDefault("Default Auto", new ArcadeDrive());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
     }
--- 2017-Robot/src/org/usfirst/frc/team6121/robot/RobotMap.java
+++ 2017-Robot/src/org/usfirst/frc/team6121/robot/RobotMap.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team6121.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,13 +11,30 @@ package org.usfirst.frc.team6121.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+	
+	/************ MOTORS ***********/
+	
+	public static final int FRONT_LEFT_MOTOR = 0;
+	public static final int REAR_LEFT_MOTOR = 1;
+	public static final int FRONT_RIGHT_MOTOR = 2;
+	public static final int REAR_RIGHT_MOTOR = 3;
+	
+	/********* CONTROLLERS *********/
+	
+	public static final int XBOX_CONTROLLER = 0;
+	
+	public static final int Y_AXIS = 1;
+	public static final int X_AXIS = 2;
+	
+	public static final int A_BUTTON = 1;
+	
+	/******* SPEED CONTROLLERS *****/
+	
+	public static SpeedController flMotor = new Talon(FRONT_LEFT_MOTOR);
+	public static SpeedController rlMotor = new Talon(REAR_LEFT_MOTOR);
+	public static SpeedController frMotor = new Talon(FRONT_RIGHT_MOTOR);
+	public static SpeedController rrMotor = new Talon(REAR_RIGHT_MOTOR);
+	
+	public static RobotDrive driveTrain = new RobotDrive(flMotor, rlMotor, frMotor, rrMotor);
+	
 }
